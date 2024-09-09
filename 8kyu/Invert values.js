a53c4671a0364207be1a15792b95bd59417d0602
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-09-09 10:37:27 +0200

    First commit

---
+++ 8kyu/Invert values.js
@@ -0,0 +1,14 @@
+/*
+Description
+
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, 
+and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+*/
+
+function invert(array) {
+  return array.map((elem) => elem * -1);
+}
---
+++ 8kyu/MakeUpperCase.js
@@ -0,0 +1,9 @@
+/*
+Description
+
+Write a function which converts the input string to uppercase.
+*/
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
